dc6e701e221aab861dc20a333a7a28a605bc4687
Enzo Nicolas Guzman Canivares <nicocanivares2002@gmail.com>
2025-10-26 23:24:07 -0300

    Creacion del repositorio y controlador de presupuestos

--- Tienda/Controllers/ProductoController.cs
+++ Tienda/Controllers/ProductoController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TiendaDB;
@@ -6,18 +7,71 @@ namespace TiendaDB;
 [Route("Tienda")]
 public class ProductoController : ControllerBase
 {
-    [HttpPost("api/Producto")]
-    public IActionResult addNewProducto(Productos productos)
+    ProductoRepository productoRepository;
+    public ProductoController()
     {
-        var productoACargar = new ProductoRepository();
-        
-        if (productoACargar.addNewProducto(productos))
+        productoRepository = new ProductoRepository();
+    }
+
+    [HttpPost("api/Producto")]
+    public IActionResult addNewProducto(Productos producto)
+    {
+
+
+        if (productoRepository.addNewProducto(producto))
         {
             return Ok("Producto Creado Correctamente!");
-        }else
+        }
+        else
         {
             return BadRequest("Ocurrio un error! No se pudo crear el producto.");
         }
     }
 
+    [HttpPut("api/Producto/{Id}")]
+    public IActionResult updateProducto(int Id, Productos producto)
+    {
+
+
+
+        if (productoRepository.updateProducto(Id, producto))
+        {
+            return Ok($"Producto con id = {Id} modificado correctamente");
+        }
+        else
+        {
+            return BadRequest("No se puedo modificar el producto!");
+        }
+
+    }
+
+    [HttpGet("api/Producto")]
+    public IActionResult getProductos()
+    {
+        var listadoProductos = productoRepository.getProductos();
+
+        return Ok(listadoProductos);
+    }
+
+    [HttpGet("api/Producto/{Id}")]
+    public IActionResult getProductosById(int Id)
+    {
+        var productoBuscado = productoRepository.getProductosById(Id);
+
+        return Ok(productoBuscado);
+
+    }
+
+    [HttpDelete("api/Producto/{Id}")]
+    public IActionResult deleteProductosById(int Id)
+    {
+        var productoElimado = productoRepository.deleteProductoById(Id);
+
+        if (productoElimado)
+        {
+            return Ok($"Producto con ID = {Id} eliminado correctamente");
+        }
+
+        return BadRequest("No se pudo eliminar el producto!");
+    }
 }
--- Tienda/Repositorios/ProductoRepository.cs
+++ Tienda/Repositorios/ProductoRepository.cs
@@ -8,7 +8,7 @@ public class ProductoRepository
     private string _connectionString = "Data Source=Tienda.db;";
 
     //Metodo para agregar un nuevo producto
-    public bool addNewProducto(Productos productos)
+    public bool addNewProducto(Productos producto)
     {
 
         using (var connection = new SqliteConnection(_connectionString))
@@ -19,8 +19,8 @@ public class ProductoRepository
 
             using (var command = new SqliteCommand(sql, connection))
             {
-                command.Parameters.AddWithValue("@desc", productos.Descripcion);
-                command.Parameters.AddWithValue("@prec", productos.Precio);
+                command.Parameters.AddWithValue("@desc", producto.Descripcion);
+                command.Parameters.AddWithValue("@prec", producto.Precio);
 
                 var filasInsertadas = command.ExecuteNonQuery();
 
@@ -36,7 +36,126 @@ public class ProductoRepository
         }
 
     }
-    
 
+    //Metodo para modificar un producto existente
+
+    public bool updateProducto(int idProducto, Productos productos)
+    {
+
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+
+            string sql = "UPDATE Productos SET Descripcion = @desc, precio = @prec WHERE idProducto = @id";
+
+            using (var command = new SqliteCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@desc", productos.Descripcion);
+                command.Parameters.AddWithValue("@prec", productos.Precio);
+
+                command.Parameters.AddWithValue("@id", idProducto);
+
+
+                var filasModificadas = command.ExecuteNonQuery();
+
+                if (filasModificadas > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+    }
+
+    public List<Productos> getProductos()
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            string sql = "SELECT idProducto,Descripcion,precio FROM Productos";
+            using (var command = new SqliteCommand(sql, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    var productos = new List<Productos>();
+                    while (reader.Read())
+                    {
+                        var producto = new Productos();
+                        //Carga de producto
+                        producto.IdProducto = Convert.ToInt32(reader["idProducto"]);
+                        producto.Descripcion = reader["Descripcion"].ToString();
+                        producto.Precio = Convert.ToInt32(reader["Precio"]);
+
+                        productos.Add(producto);
+
+                    }
+                    return productos;
+                }
+            }
+        }
+
+    }
+
+    public Productos getProductosById(int idProducto)
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            string sql = "SELECT idProducto,Descripcion,precio FROM Productos where idProducto = @idProd";
+            using (var command = new SqliteCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@idProd", idProducto);
+                using (var reader = command.ExecuteReader())
+                {
+
+                    if (reader.Read())
+                    {
+                        var productoBuscado = new Productos();
+
+                        //Carga de producto
+                        productoBuscado.IdProducto = Convert.ToInt32(reader["idProducto"]);
+                        productoBuscado.Descripcion = reader["Descripcion"].ToString();
+                        productoBuscado.Precio = Convert.ToInt32(reader["Precio"]);
+
+                        return productoBuscado;
+                    }
+
+                    return null;
+
+                }
+            }
+        }
+
+    }
+
+    public bool deleteProductoById(int idProducto)
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+
+            connection.Open();
+
+            var sql = "DELETE FROM Productos WHERE idProducto = @idProd";
+
+            using (var command = new SqliteCommand(sql,connection))
+            {
+                command.Parameters.AddWithValue("@idProd", idProducto);
+
+
+
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
 
 }
